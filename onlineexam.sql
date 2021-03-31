329e6f13d430cd7783b160be6c0bbe6545fca5c1
JosephLG4535 <78837456+JosephLG4535@users.noreply.github.com>
2021-04-01 07:52:06 +0800

    Update onlineexam.sql

--- onlineexam.sql
+++ onlineexam.sql
@@ -1,21 +1,20 @@
 -- phpMyAdmin SQL Dump
--- version 5.0.4
--- https://www.phpmyadmin.net/
+-- version 4.2.11
+-- http://www.phpmyadmin.net
 --
 -- Host: 127.0.0.1
--- Generation Time: Mar 05, 2021 at 04:49 PM
--- Server version: 10.4.17-MariaDB
--- PHP Version: 8.0.1
+-- Generation Time: Apr 01, 2021 at 01:47 AM
+-- Server version: 5.6.21
+-- PHP Version: 5.6.3
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-START TRANSACTION;
 SET time_zone = "+00:00";
 
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
+/*!40101 SET NAMES utf8 */;
 
 --
 -- Database: `onlineexam`
@@ -27,7 +26,7 @@ SET time_zone = "+00:00";
 -- Table structure for table `admin`
 --
 
-CREATE TABLE `admin` (
+CREATE TABLE IF NOT EXISTS `admin` (
   `adminID` varchar(10) NOT NULL,
   `adminName` varchar(40) NOT NULL,
   `email` varchar(50) NOT NULL,
@@ -50,7 +49,7 @@ INSERT INTO `admin` (`adminID`, `adminName`, `email`, `password_hash`) VALUES
 -- Table structure for table `course`
 --
 
-CREATE TABLE `course` (
+CREATE TABLE IF NOT EXISTS `course` (
   `courseID` varchar(10) NOT NULL,
   `courseName` varchar(60) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
@@ -76,8 +75,8 @@ INSERT INTO `course` (`courseID`, `courseName`) VALUES
 -- Table structure for table `exam`
 --
 
-CREATE TABLE `exam` (
-  `examID` int(11) NOT NULL,
+CREATE TABLE IF NOT EXISTS `exam` (
+`examID` int(11) NOT NULL,
   `examTitle` varchar(50) NOT NULL,
   `courseID` varchar(10) NOT NULL,
   `date` date NOT NULL,
@@ -87,7 +86,7 @@ CREATE TABLE `exam` (
   `rightAnsMark` float NOT NULL,
   `wrongAnsMark` float NOT NULL,
   `teacherID` varchar(30) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `exam`
@@ -104,7 +103,7 @@ INSERT INTO `exam` (`examID`, `examTitle`, `courseID`, `date`, `time`, `duration
 -- Table structure for table `faculty`
 --
 
-CREATE TABLE `faculty` (
+CREATE TABLE IF NOT EXISTS `faculty` (
   `facultyID` varchar(10) NOT NULL,
   `facultyName` varchar(60) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
@@ -122,11 +121,34 @@ INSERT INTO `faculty` (`facultyID`, `facultyName`) VALUES
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `mark`
+--
+
+CREATE TABLE IF NOT EXISTS `mark` (
+`markID` int(11) NOT NULL,
+  `studentID` varchar(10) NOT NULL,
+  `examID` int(11) NOT NULL,
+  `totalMarks` float NOT NULL,
+  `ranking` int(11) NOT NULL
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `mark`
+--
+
+INSERT INTO `mark` (`markID`, `studentID`, `examID`, `totalMarks`, `ranking`) VALUES
+(1, 'student02', 1, 4.5, 1),
+(2, 'student07', 2, 2.5, 1),
+(3, 'student02', 2, 1.5, 2);
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `question`
 --
 
-CREATE TABLE `question` (
-  `questionID` int(11) NOT NULL,
+CREATE TABLE IF NOT EXISTS `question` (
+`questionID` int(11) NOT NULL,
   `examID` int(11) NOT NULL,
   `question` varchar(100) NOT NULL,
   `option1` varchar(50) NOT NULL,
@@ -134,7 +156,7 @@ CREATE TABLE `question` (
   `option3` varchar(50) NOT NULL,
   `option4` varchar(50) NOT NULL,
   `answer` varchar(50) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=25 DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `question`
@@ -170,8 +192,8 @@ INSERT INTO `question` (`questionID`, `examID`, `question`, `option1`, `option2`
 -- Table structure for table `result`
 --
 
-CREATE TABLE `result` (
-  `resultID` int(11) NOT NULL,
+CREATE TABLE IF NOT EXISTS `result` (
+`resultID` int(11) NOT NULL,
   `studentID` varchar(10) NOT NULL,
   `examID` int(11) DEFAULT NULL,
   `questionID` int(11) NOT NULL,
@@ -179,7 +201,7 @@ CREATE TABLE `result` (
   `correctAns` varchar(50) NOT NULL,
   `result` varchar(10) NOT NULL,
   `marks` float NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=25 DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `result`
@@ -205,7 +227,11 @@ INSERT INTO `result` (`resultID`, `studentID`, `examID`, `questionID`, `studentA
 (17, 'student07', 2, 17, 'digestion', 'peristaltic movement', 'Wrong', -1.5),
 (18, 'student07', 2, 18, 'anal sphincter', 'anal sphincter', 'Right', 1),
 (19, 'student07', 2, 19, 'Carbon dioxide + Water + Energy', 'Ethanol + Carbon dioxide + Energy', 'Wrong', -1.5),
-(20, 'student07', 2, 20, 'mitochondria', 'mitochondria', 'Right', 1);
+(20, 'student07', 2, 20, 'mitochondria', 'mitochondria', 'Right', 1),
+(21, 'student02', 2, 11, 'Carbohydrates and fats', 'Carbohydrates and fats', 'Right', 1),
+(22, 'student02', 2, 12, 'Autotrophic nutrition', 'Saprotrophic nutrition', 'Wrong', -1.5),
+(23, 'student02', 2, 14, 'chloroplast', 'chloroplast', 'Right', 1),
+(24, 'student02', 2, 15, ' food vacuole', ' food vacuole', 'Right', 1);
 
 -- --------------------------------------------------------
 
@@ -213,7 +239,7 @@ INSERT INTO `result` (`resultID`, `studentID`, `examID`, `questionID`, `studentA
 -- Table structure for table `student`
 --
 
-CREATE TABLE `student` (
+CREATE TABLE IF NOT EXISTS `student` (
   `studentID` varchar(10) NOT NULL,
   `studentName` varchar(40) NOT NULL,
   `email` varchar(50) NOT NULL,
@@ -245,7 +271,7 @@ INSERT INTO `student` (`studentID`, `studentName`, `email`, `courseID`, `passwor
 -- Table structure for table `teacher`
 --
 
-CREATE TABLE `teacher` (
+CREATE TABLE IF NOT EXISTS `teacher` (
   `teacherID` varchar(10) NOT NULL,
   `teacherName` varchar(40) NOT NULL,
   `facultyID` varchar(10) NOT NULL,
@@ -281,11 +307,11 @@ INSERT INTO `teacher` (`teacherID`, `teacherName`, `facultyID`, `email`, `passwo
 -- Table structure for table `teacher_course`
 --
 
-CREATE TABLE `teacher_course` (
-  `tc_course` int(30) NOT NULL,
+CREATE TABLE IF NOT EXISTS `teacher_course` (
+`tc_course` int(30) NOT NULL,
   `teacherID` varchar(30) NOT NULL,
   `courseID` varchar(30) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4;
 
 --
 -- Dumping data for table `teacher_course`
@@ -305,64 +331,61 @@ INSERT INTO `teacher_course` (`tc_course`, `teacherID`, `courseID`) VALUES
 -- Indexes for table `admin`
 --
 ALTER TABLE `admin`
-  ADD PRIMARY KEY (`adminID`);
+ ADD PRIMARY KEY (`adminID`);
 
 --
 -- Indexes for table `course`
 --
 ALTER TABLE `course`
-  ADD PRIMARY KEY (`courseID`);
+ ADD PRIMARY KEY (`courseID`);
 
 --
 -- Indexes for table `exam`
 --
 ALTER TABLE `exam`
-  ADD PRIMARY KEY (`examID`),
-  ADD KEY `courseID` (`courseID`);
+ ADD PRIMARY KEY (`examID`), ADD KEY `courseID` (`courseID`);
 
 --
 -- Indexes for table `faculty`
 --
 ALTER TABLE `faculty`
-  ADD PRIMARY KEY (`facultyID`);
+ ADD PRIMARY KEY (`facultyID`);
+
+--
+-- Indexes for table `mark`
+--
+ALTER TABLE `mark`
+ ADD PRIMARY KEY (`markID`), ADD KEY `studentID` (`studentID`), ADD KEY `examID` (`examID`);
 
 --
 -- Indexes for table `question`
 --
 ALTER TABLE `question`
-  ADD PRIMARY KEY (`questionID`),
-  ADD KEY `examID` (`examID`);
+ ADD PRIMARY KEY (`questionID`), ADD KEY `examID` (`examID`);
 
 --
 -- Indexes for table `result`
 --
 ALTER TABLE `result`
-  ADD PRIMARY KEY (`resultID`),
-  ADD KEY `studentID` (`studentID`),
-  ADD KEY `questionID` (`questionID`),
-  ADD KEY `examID` (`examID`) USING BTREE;
+ ADD PRIMARY KEY (`resultID`), ADD KEY `studentID` (`studentID`), ADD KEY `questionID` (`questionID`), ADD KEY `examID` (`examID`) USING BTREE;
 
 --
 -- Indexes for table `student`
 --
 ALTER TABLE `student`
-  ADD PRIMARY KEY (`studentID`),
-  ADD KEY `courseID` (`courseID`);
+ ADD PRIMARY KEY (`studentID`), ADD KEY `courseID` (`courseID`);
 
 --
 -- Indexes for table `teacher`
 --
 ALTER TABLE `teacher`
-  ADD PRIMARY KEY (`teacherID`),
-  ADD KEY `facultyID` (`facultyID`);
+ ADD PRIMARY KEY (`teacherID`), ADD KEY `facultyID` (`facultyID`);
 
 --
 -- Indexes for table `teacher_course`
 --
 ALTER TABLE `teacher_course`
-  ADD PRIMARY KEY (`tc_course`),
-  ADD KEY `teacherID` (`teacherID`),
-  ADD KEY `courseID` (`courseID`);
+ ADD PRIMARY KEY (`tc_course`), ADD KEY `teacherID` (`teacherID`), ADD KEY `courseID` (`courseID`);
 
 --
 -- AUTO_INCREMENT for dumped tables
@@ -372,26 +395,27 @@ ALTER TABLE `teacher_course`
 -- AUTO_INCREMENT for table `exam`
 --
 ALTER TABLE `exam`
-  MODIFY `examID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
-
+MODIFY `examID` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=4;
+--
+-- AUTO_INCREMENT for table `mark`
+--
+ALTER TABLE `mark`
+MODIFY `markID` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=4;
 --
 -- AUTO_INCREMENT for table `question`
 --
 ALTER TABLE `question`
-  MODIFY `questionID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=25;
-
+MODIFY `questionID` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=25;
 --
 -- AUTO_INCREMENT for table `result`
 --
 ALTER TABLE `result`
-  MODIFY `resultID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=21;
-
+MODIFY `resultID` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=25;
 --
 -- AUTO_INCREMENT for table `teacher_course`
 --
 ALTER TABLE `teacher_course`
-  MODIFY `tc_course` int(30) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
-
+MODIFY `tc_course` int(30) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=5;
 --
 -- Constraints for dumped tables
 --
@@ -400,34 +424,40 @@ ALTER TABLE `teacher_course`
 -- Constraints for table `exam`
 --
 ALTER TABLE `exam`
-  ADD CONSTRAINT `exam_ibfk_1` FOREIGN KEY (`courseID`) REFERENCES `course` (`courseID`);
+ADD CONSTRAINT `exam_ibfk_1` FOREIGN KEY (`courseID`) REFERENCES `course` (`courseID`);
+
+--
+-- Constraints for table `mark`
+--
+ALTER TABLE `mark`
+ADD CONSTRAINT `mark_ibfk_1` FOREIGN KEY (`studentID`) REFERENCES `student` (`studentID`),
+ADD CONSTRAINT `mark_ibfk_2` FOREIGN KEY (`examID`) REFERENCES `exam` (`examID`);
 
 --
 -- Constraints for table `question`
 --
 ALTER TABLE `question`
-  ADD CONSTRAINT `question_ibfk_1` FOREIGN KEY (`examID`) REFERENCES `exam` (`examID`);
+ADD CONSTRAINT `question_ibfk_1` FOREIGN KEY (`examID`) REFERENCES `exam` (`examID`);
 
 --
 -- Constraints for table `result`
 --
 ALTER TABLE `result`
-  ADD CONSTRAINT `result_ibfk_1` FOREIGN KEY (`studentID`) REFERENCES `student` (`studentID`),
-  ADD CONSTRAINT `result_ibfk_2` FOREIGN KEY (`questionID`) REFERENCES `question` (`questionID`),
-  ADD CONSTRAINT `result_ibfk_3` FOREIGN KEY (`examID`) REFERENCES `exam` (`examID`);
+ADD CONSTRAINT `result_ibfk_1` FOREIGN KEY (`studentID`) REFERENCES `student` (`studentID`),
+ADD CONSTRAINT `result_ibfk_2` FOREIGN KEY (`questionID`) REFERENCES `question` (`questionID`),
+ADD CONSTRAINT `result_ibfk_3` FOREIGN KEY (`examID`) REFERENCES `exam` (`examID`);
 
 --
 -- Constraints for table `student`
 --
 ALTER TABLE `student`
-  ADD CONSTRAINT `student_ibfk_1` FOREIGN KEY (`courseID`) REFERENCES `course` (`courseID`);
+ADD CONSTRAINT `student_ibfk_1` FOREIGN KEY (`courseID`) REFERENCES `course` (`courseID`);
 
 --
 -- Constraints for table `teacher`
 --
 ALTER TABLE `teacher`
-  ADD CONSTRAINT `teacher_ibfk_1` FOREIGN KEY (`facultyID`) REFERENCES `faculty` (`facultyID`);
-COMMIT;
+ADD CONSTRAINT `teacher_ibfk_1` FOREIGN KEY (`facultyID`) REFERENCES `faculty` (`facultyID`);
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
