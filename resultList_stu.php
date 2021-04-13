779b8336c17c6e5fa148db78b24a1333ef774005
Iris2000 <43512172+Iris2000@users.noreply.github.com>
2021-04-13 20:58:58 +0800

    update student result list

--- resultList_stu.php
+++ resultList_stu.php
@@ -17,11 +17,9 @@
             <thead>
                 <tr>
                 <th scope="col">#</th>
-                <th scope="col">Student ID</th>
-                <th scope="col">Student Name</th>
+                <th scope="col">Exam Taken</th>
                 <th scope="col">Total Marks Obtained</th>
                 <th scope="col">Ranking</th>
-                <th scope="col">Exam Taken</th>
                 <th scope="col">Date / Time</th>
                 <th scope="col">Details</th>
                 </tr>
@@ -37,14 +35,9 @@
                     do {
                         $id = $resultRow['examID'];
                         echo "<tr><td>$i</td>";
-                        echo "<td>".$resultRow['studentID']."</td>";
-                        echo "<td>".$resultRow['studentName']."</td>";
-                        echo "<td>".$resultRow['totalMarks']."</td>";
-                        //$query2 = "SELECT * FROM mark WHERE exam";
-                        //$rankQuery = mysqli_query($conn, $query);
-                        //$rankRow = mysqli_fetch_assoc($resultQuery);
-                        echo "<td>".$resultRow['ranking']."</td>";
                         echo "<td>".$resultRow['examTitle']."</td>";
+                        echo "<td>".$resultRow['totalMarks']."</td>";
+                        echo "<td>".$resultRow['ranking']."</td>";
                         echo "<td>".$resultRow['date']." / ".$resultRow['time']."</td>";
                         echo "<form action='detailList_stu.php?id=$id' method='post'>";
                         echo "<td><input type='submit' class='btn btn-primary' name='detail' value='Detail'/></td></form></tr>\n";
